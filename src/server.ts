eb4c48709d54136e0d9348fd4e1368ebe8c6d897
Dieter Jackson <jacksondieter@gmail.com>
2021-06-17 01:34:14 +0200

    Inital configuration

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
+
+const app: express.Application = express()
+const address = '0.0.0.0:3000'
+
+app.use(bodyParser.json())
+
+app.get('/', function (req: Request, res: Response) {
+	res.send('Home')
+})
+
+app.listen(3000, function () {
+  console.log(`starting app on: ${address}`)
+})
+
+export default app
